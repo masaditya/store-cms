8eac8bc4c8296501d4335a905e4fb9aabf0221b0
Aditya Eka <aeka489@gmail.com>
2020-10-08 18:47:44 +0700

    Testing2 (#2)
    * test actions
    
    * fix test
    
    * update(App) : sungguh terlalu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There!</h1>
+      <h2>Sungguh terlalu</h2>
     </div>
   );
 }
